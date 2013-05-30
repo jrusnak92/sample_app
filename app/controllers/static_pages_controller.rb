e7093105ab4a8d4ba81073b335330f7f13d4b7f9
Jonathan Rusnak <jrusnak92@gmail.com>
2013-05-29 23:57:05 -0700

    Final Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,9 @@ class StaticPagesController < ApplicationController
   def help
     
   end
+  
+  def about
+    
+  end
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do #description of the test belongs here
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
@@ -12,7 +12,7 @@ describe "Static pages" do
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do #description of the test belongs here
+    it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
