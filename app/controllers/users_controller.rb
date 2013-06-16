a5c710e73182709ba16ac9bc945724d50c6bbaac
Jonathan Rusnak <jrusnak92@gmail.com>
2013-06-16 00:06:03 -0700

    Attempt at edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,15 +28,7 @@ class UsersController < ApplicationController
   def edit
   end
   
-  def update
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
-      sign_in @user
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
+
   
     private
 
@@ -61,5 +53,14 @@ class UsersController < ApplicationController
   	def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
-  
+    
+    def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
